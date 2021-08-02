ded7e35a3f666f0a68c3e376967480ba764efa57
garrettcook <38377582+garrettcook@users.noreply.github.com>
2021-08-02 08:16:38 -0400

    Update pella_insynctive.groovy

--- pella_insynctive.groovy
+++ pella_insynctive.groovy
@@ -4,6 +4,7 @@ Pella Insynctive Telnet Driver
 v1.0 - initial release
 v1.0.1 - fix issue with connectivity check
 v1.0.2 - rebuildChildDevices without deleting and re-adding all -- preserve existing devices
+v1.0.3 - fix issue with connectivity check, embed child device names in file (and write to device label on refresh), miscellaneous bug fixes and changes to work with newer Hubitat firmwares.
 
 */
 
@@ -36,23 +37,27 @@ metadata {
 
 def initialize()
 {
-	if (ipAddress == null || ipAddress == "") {
-		displayDebugLog("Cannot Connect: IP Address is required")
-		return false
+	if (!parent) {
+		if (ipAddress == null || ipAddress == "") {
+			displayDebugLog("Cannot Connect: IP Address is required")
+			return false
+		}
+		def hub = location.hubs[0]
+		displayDebugLog("Hub Uptime: ${hub.uptime}") /* (uptime may impact ability to connect to telnet if hubitat was just started up.  May be useful for setting a delay if attempts to connect to telnet too soon) */
+		if (hub.uptime < 120)
+			pauseExecution(120000) /* wait for 2 minutes if the hub has not been running for at least two minutes -- allows telnet services to load on the hub and also allows Pella bridge time to boot up e.g. if the restart was due to a power outage */
+		telnetClose() /* closes telnet if it was already open */
+		displayInfoLog("Opening telnet connection")
+		telnetConnect("${ipAddress}", 23, null, null)
+		displayInfoLog("Establishing connection")
+		pauseExecution(3000)
+		sendMessage('?POINTCOUNT', null) /* test connection by getting a simple count of the number of devices connected to the Pella Bridge */
+		pauseExecution(2000)
+		schedule("0 */4 * ? * * *", evalConnection) /* test connection every four minutes.  By default, any such previous schedule is overwritten */
+		return true
+	} else {
+		unschedule()
 	}
-	def hub = location.hubs[0]
-	displayDebugLog("Hub Uptime: ${hub.uptime}") /* (uptime may impact ability to connect to telnet if hubitat was just started up.  May be useful for setting a delay if attempts to connect to telnet too soon) */
-	if (hub.uptime < 120)
-		pauseExecution(120000) /* wait for 2 minutes if the hub has not been running for at least two minutes -- allows telnet services to load on the hub and also allows Pella bridge time to boot up e.g. if the restart was due to a power outage */
-	telnetClose() /* closes telnet if it was already open */
-	displayInfoLog("Opening telnet connection")
-	telnetConnect("${ipAddress}", 23, null, null)
-    displayInfoLog("Establishing connection")
-	pauseExecution(3000)
-	sendMessage('?POINTCOUNT', null) /* test connection by getting a simple count of the number of devices connected to the Pella Bridge */
-	pauseExecution(1000)
-	schedule("0 */4 * ? * * *", evalConnection) /* test connection every four minutes.  By default, any such previous schedule is overwritten */
-	return true
 }
 
 def reconnect()
@@ -67,7 +72,7 @@ def reconnect()
 def evalConnection()
 {
 	sendMessage('?POINTCOUNT', null) /* test connection by getting a simple count of the number of devices connected to the Pella Bridge */
-	pauseExecution(1000)
+	pauseExecution(2000)
 	if (device.currentValue('lastMessageReceipt')) {
 		// duration = groovy.time.TimeCategory.minus(
 			// new Date(),
@@ -108,9 +113,14 @@ def refresh()
 	def matches = (device.deviceNetworkId =~ /\d+$/) /* grabs digits at end of deviceNetworkId */
 	def firstmatch = matches[0]
 	if (parent) {
+		device.deleteCurrentState("lastMessageReceipt")
+		device.deleteCurrentState("numberOfDevices")
+		device.deleteCurrentState("rebootBridge")
 		parent.sendMessage("?POINTSTATUS-${firstmatch}", firstmatch)
 		pauseExecution(500) /* wait 0.5 seconds before sending the next command */
 		parent.sendMessage("?POINTBATTERYGET-${firstmatch}", firstmatch)
+		pauseExecution(500) /* wait 0.5 seconds before sending the next command */
+		parent.sendMessage("?POINTID-${firstmatch}", firstmatch)
 	} else {
 		return
 	}
@@ -130,6 +140,8 @@ def parse(String message) {
 			parseStatusResponse(state.lastMessageSent, message)
 		} else if (state.lastMessageSent.contains("?POINTBATTERYGET") && (message =~ /^\W*\$[0-9A-F]{2}\W*$/)) {
 			parseBatteryResponse(state.lastMessageSent, message)
+		} else if (state.lastMessageSent.contains("?POINTID") && (message =~ /^\W*\$[0-9A-F]{6}\W*$/)) {
+			parseIDResponse(state.lastMessageSent, message)
 		} else if (state.lastMessageSent == "?POINTCOUNT" && (message =~ /^\W*\d{3}\W*$/)) {
 			parseDevices(message)
 		} else if (message.contains("Insynctive Telnet Server")) {
@@ -211,6 +223,49 @@ def parseBatteryResponse(lastMessageSent, message) {
 	}
 }
 
+def parseIDResponse(lastMessageSent, message) {
+	displayDebugLog("ID string to parse = '${message}'")
+	def matches = ("${lastMessageSent}" =~ /(?<=\-)\d{3}\W*$/) /* grabs integers of Pella device number */
+	def firstmatch = matches[0]
+	def IDmatches = ("${message}" =~ /[0-9A-F]+/) /* grabs reported serial number */
+	def firstIDmatch = IDmatches[0]
+	def descText = "ID is ${firstIDmatch}"
+	displayDebugLog("Device to send state to = $device.deviceNetworkId-$firstmatch")
+	def childDevice = childDevices.find{it.deviceNetworkId == "$device.deviceNetworkId-$firstmatch"}
+	if (childDevice) {
+		childDevice.sendEvent(name: "ID", value: "${firstIDmatch}", displayed: true)
+		switch(firstIDmatch) {
+			case "680925":
+				childDevice.setLabel("Front Door"); 
+				break; 
+			case "680957": 
+				childDevice.setLabel("Deck Door"); 
+				break; 
+			case "6808FE": 
+				childDevice.setLabel("Garage Door"); 
+				break; 
+			case "6808EB": 
+				childDevice.setLabel("North Patio Door"); 
+				break; 
+			case "680960": 
+				childDevice.setLabel("South Patio Door"); 
+				break; 
+			case "082404": 
+				childDevice.setLabel("North Basement Window"); 
+				break; 
+			case "082452": 
+				childDevice.setLabel("South Basement Window"); 
+				break; 
+			case "0825C9": 
+				childDevice.setLabel("Dining Room Window"); 
+				break; 
+			case "080C2C": 
+				childDevice.setLabel("Crawlspace Door"); 
+				break; 
+		}
+	}
+}
+
 def installed() {
 	// displayDebugLog("Installing")
     // createChildDevices()
@@ -235,15 +290,17 @@ def rebuildChildDevices() {
 }
 
 def install() {
-	if (initialize() == false) {
-		displayDebugLog("Cannot Connect")
-		return
-	}
-    displayDebugLog("Creating new child devices")
-    for (i in 1..(device.currentValue('numberOfDevices'))) {
-        try {
-			addChildDevice('hubitat', "Pella Insynctive", "$device.deviceNetworkId-${i.toString().padLeft(3, "0")}", [name: "Pella Insynctive Device", isComponent: true])
-		} catch(Exception ex) {
+	if (!parent) {
+		if (initialize() == false) {
+			displayDebugLog("Cannot Connect")
+			return
+		}
+		displayDebugLog("Creating new child devices")
+		for (i in 1..(device.currentValue('numberOfDevices'))) {
+			try {
+				addChildDevice('hubitat', "Pella Insynctive", "$device.deviceNetworkId-${i.toString().padLeft(3, "0")}", [name: "Pella Insynctive Device", isComponent: true])
+			} catch(Exception ex) {
+			}
 		}
 	}
 }
